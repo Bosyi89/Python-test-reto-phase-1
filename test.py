7d770423cecf2d20a304bcb548f0266b2d523803
Serhii <62995921+Bosyi89@users.noreply.github.com>
2020-04-02 20:36:08 -0400

    Update test.py

---
+++ test.py
@@ -0,0 +1,50 @@
+# Наш перший коментар
+inv_value = 1 # Ціле число, integer or int
+negative_int_value = -1 # Ціле відємне число, integer or int
+print(inv_value)
+print(negative_int_value)
+
+float_value = 1.234 #Float, дробове число з плаваючою точкою
+print(float_value)
+
+string_value = '1' #string or str стрічка
+print(string_value)
+
+string_second = 'Text 2'
+print(string_second)
+
+meaningful_sentence = "Once upon" +\
+"time .... don't"
+print(meaningful_sentence)
+
+paragraph_text = """First line
+second line
+third line
+...
+last line"""
+print(paragraph_text)
+
+negative_statement = False
+positive_statement = True
+
+print(negative_statement)
+print(positive_statement)
+
+x = 1
+y = 2
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+print(x % y)
+
+x = 2
+y = 3
+Comparing_result = x == y
+
+print(f'Comparing {x} and {y}: {Comparing_result}')
+
+
+
+
+
