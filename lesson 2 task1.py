970eec43715018654f9cb65b012c028cfbc17c53
Serhii <62995921+Bosyi89@users.noreply.github.com>
2020-04-13 17:53:51 -0400

    Create lesson 2 task1.py

---
+++ lesson 2 task1.py
@@ -0,0 +1,6 @@
+a = 'serhii'
+b = 'monday'
+print('Good day', a,'!', b, 'is a perfect day to learn some python')
+print()
+a, b = 'serhii', 'monday'
+print('Good day', a,'!', b, 'is a perfect day to learn some python')
