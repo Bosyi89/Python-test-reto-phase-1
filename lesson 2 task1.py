10aa2e066126921808e14c49ff8b618593db0af7
Serhii <62995921+Bosyi89@users.noreply.github.com>
2020-04-16 17:26:23 -0400

    Update lesson 2 task1.py

--- lesson 2 task1.py
+++ lesson 2 task1.py
@@ -4,3 +4,6 @@ print('Good day', a,'!', b, 'is a perfect day to learn some python')
 print()
 a, b = 'serhii', 'monday'
 print('Good day', a,'!', b, 'is a perfect day to learn some python')
+print('Good day', a,'!', b, 'is a perfect day to learn some python.')
+print(f'Good day{a}! {b} is a perfect day to learn some python.')
+print('Good day{}! {} is a perfect day to learn some python'.format(a, b))
