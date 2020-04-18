00b1c97f363ccc951957edda18899c13058a13c3
Serhii <62995921+Bosyi89@users.noreply.github.com>
2020-04-17 20:06:57 -0400

    Create Lesson 3 task2.py

---
+++ Lesson 3 task2.py
@@ -0,0 +1,7 @@
+a = input('Please, write your number use only digits :')
+if a.isdigit() is not True:
+    print(a, 'Your number must be only with digits')
+elif len(a) < 10:
+	print('your number should be 10 digits')
+else:
+	print('your phone number is correct', a)
