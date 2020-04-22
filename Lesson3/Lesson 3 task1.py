9bb1dc123d56316f62aedb2cbd7c89298f4743ba
Serhii <62995921+Bosyi89@users.noreply.github.com>
2020-04-22 13:25:35 -0400

    Create Lesson 3 task1.py

---
+++ Lesson3/Lesson 3 task1.py
@@ -0,0 +1,21 @@
+
+#first 2 and the last 2 chars from a given string. 
+#If the string length is less than 2, return instead of the empty string.
+#Sample String: 'helloworld'
+#Expected Result : 'held'
+
+#Sample String: 'my'
+#Expected Result : 'mymy'
+
+#Sample String: ' x'
+#Expected Result: Empty String
+# use only []
+
+a = input('Name:').strip()
+if len(a) > 2:
+    print(a[0:2] + a[-2] + a[-1])
+if len(a) == 2:
+    print(a[0:2] + a[-2] + a[-1])
+if len(a) < 2:
+    print('Empty String')
+    
