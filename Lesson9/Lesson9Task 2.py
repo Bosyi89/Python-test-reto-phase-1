2995ece35bc1cc4c0f66c7889f25a53a0bd31b3a
Serhii <62995921+Bosyi89@users.noreply.github.com>
2020-05-11 14:54:35 -0400

    Create Lesson9Task 2.py

---
+++ Lesson9/Lesson9Task 2.py
@@ -0,0 +1,9 @@
+import json
+
+def phone_book(our_phone_book, first_name, last_name, full_name, number, city):
+    number_phone = {'our_phone_book' : phonebook,
+                    'first_name': first_name,
+                    'last_name' : last_name,
+                    'full_name' : full_name,
+                    'number': number,
+                    'city' : city}
